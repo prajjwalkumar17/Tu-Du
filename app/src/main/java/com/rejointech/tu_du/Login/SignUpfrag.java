728362648ffca5e98fd25f6d2fa8949ed595fd58
pk <talk2prajjwal@gmail.com>
2020-12-03 00:27:06 +0530

    norm

---
+++ app/src/main/java/com/rejointech/tu_du/Login/SignUpfrag.java
@@ -0,0 +1,22 @@
+package com.rejointech.tu_du.Login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rejointech.tu_du.R;
+
+
+public class SignUpfrag extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root=inflater.inflate(R.layout.fragment_sign_upfrag, container, false);
+        return root;
+    }
+}
