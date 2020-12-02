2be6f0f4435f0ddef05ff5922434d5310ceabef0
Prajjwal kumar <hangsai@Prajjwals-MacBook-Air.local>
2020-12-03 00:12:43 +0530

    Initial commit

---
+++ app/src/main/java/com/rejointech/tu_du/Login/Splashfrag.java
@@ -0,0 +1,24 @@
+package com.rejointech.tu_du.Login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rejointech.tu_du.R;
+
+public class Splashfrag extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View root=inflater.inflate(R.layout.fragment_splashfrag, container, false);
+
+
+        return root;
+    }
+}
